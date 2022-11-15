76a59661391bfcc3d27b964c7f9d4f633736203e
Peter Hagenson <p.hagenson@gmail.com>
2022-11-15 10:24:59 -0600

    changed types from inline to type object

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,6 +1,9 @@
-import PropTypes from 'prop-types';
+type ControlPanelProps = {
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
 
-const ControlPanel = (props: { name: string; onChange: any }) => {
+const ControlPanel = ({ name, onChange }: ControlPanelProps) => {
   return (
     <form
       className="bg-primary-10 flex flex-row gap-4 text-white"
@@ -12,17 +15,12 @@ const ControlPanel = (props: { name: string; onChange: any }) => {
           name="name"
           className="w-full"
           type="text"
-          value={props.name}
-          onChange={props.onChange}
+          value={name}
+          onChange={onChange}
         />
       </div>
     </form>
   );
-};
-
-ControlPanel.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default ControlPanel;
